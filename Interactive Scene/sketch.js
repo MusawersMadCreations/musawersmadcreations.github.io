9f63a6a69aeab557620eaebf888b0986ee3aeea8
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-02-09 14:43:34 -0600

    interactive sence add

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,34 @@
+// mouse and keyboard interaction
+// Dan Schellenberg
+// Feb 8, 2018
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  document.addEventListener("contextmenu", event => event.preventDefault())
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+
+  if (mouseButton === LEFT) {
+    rect(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+  else if (mouseButton === RIGHT) {
+    ellipse(mouseX, mouseY, random(50, 200), random(50, 200));
+  }
+}
+
+function keyPressed() {
+  if (key === 'w' || key === 'W') {
+    background(255);
+  }
+  else if (key === 'b' || key === 'B') {
+    background(0);
+  }
+}
