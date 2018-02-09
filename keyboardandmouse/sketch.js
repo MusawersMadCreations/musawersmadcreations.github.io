f424d4d5fbeeebb4a5e2adfe4459d7475690598a
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-02-09 14:18:38 -0600

    4435

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -1,34 +1,34 @@
-// Wierd Wepsite
-// Musawer
+// mouse and keyboard interaction
+// Dan Schellenberg
 // Feb 8, 2018
-
-// global variables
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
+  document.addEventListener("contextmenu", event => event.preventDefault())
 }
 
 function draw() {
+
 }
 
 function mousePressed() {
-  noStroke()
-  fill(random(255),random(255),random(255),random(255))
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
 
   if (mouseButton === LEFT) {
-    rect(mouseX, mouseY, random(100), random(100));
+    rect(mouseX, mouseY, random(50, 150), random(50, 150));
   }
   else if (mouseButton === RIGHT) {
-    ellipse(mouseX, mouseY, random(100), random(100));
+    ellipse(mouseX, mouseY, random(50, 150), random(50, 150));
   }
 }
 
 function keyPressed() {
-  if (key === "w") || (key === "W") {
+  if (key === 'w' || key === 'W') {
     background(255);
   }
-  else if (key === "b") || (key === "B") {
+  else if (key === 'b' || key === 'B') {
     background(0);
   }
 }
