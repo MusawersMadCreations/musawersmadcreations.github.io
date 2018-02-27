916b3ca1a76017f0187e3df68cb9ea987fe41a0b
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-02-26 23:15:47 -0600

    2

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -9,14 +9,14 @@
 // Pause Music with "M" and Resume with "N"
 // Press any other key to stop NyanCat
 
-let catx,caty;
-let nyanCat,star,star2;
-let redChange,greenChange,blueChange;
+let catx, caty;
+let nyanCat, star, star2;
+let redChange, greenChange, blueChange;
 let catSpeed;
 let state;
 
 
-function preload(){
+function preload() {
   nyanCat = loadImage("images/cat.png");
   star = loadImage("images/star.png");
   star2 = loadImage("images/star2.png");
@@ -28,8 +28,8 @@ function setup() {
   playThemeSong();
   createCanvas(windowWidth, windowHeight);
   document.addEventListener("contextmenu", event => event.preventDefault())
-  catx = width/2 - 150;
-  caty = height/2;
+  catx = width / 2 - 150;
+  caty = height / 2;
   redChange = 0;
   greenChange = 0;
   blueChange = 0;
@@ -69,55 +69,51 @@ function playThemeSong() {
 
 function makeStars() {
   if (mouseButton === LEFT) {
-    image(star,random(width),random(height));
-  }
-  else if (mouseButton === RIGHT) {
-    image(star2,random(width),random(height));
+    image(star, random(width), random(height));
+  } else if (mouseButton === RIGHT) {
+    image(star2, random(width), random(height));
   }
 }
 
 function nyanCatMove() {
-  image(nyanCat,catx,caty,250,100);
+  image(nyanCat, catx, caty, 250, 100);
 
   if (key == 'w' || key == 'W') {
-   caty = caty - catSpeed;
-   if (caty < -10){
-     caty = height - 110;
-   }
- }
- else if (key == 's' || key == 'S') {
-   caty = caty + catSpeed;
-   if (caty >= height - 110){
-     caty = -10;
-   }
- }
- if (key == 'a' || key == 'A') {
-   catx = catx - catSpeed;
-   if (catx < 0){
-     catx = width - 260;
-   }
- }
- else if (key == 'd' || key == 'D') {
-   catx = catx + catSpeed;
-   if (catx >= width - 260){
-     catx = 0;
-   }
- }
+    caty = caty - catSpeed;
+    if (caty < -10) {
+      caty = height - 110;
+    }
+  } else if (key == 's' || key == 'S') {
+    caty = caty + catSpeed;
+    if (caty >= height - 110) {
+      caty = -10;
+    }
+  }
+  if (key == 'a' || key == 'A') {
+    catx = catx - catSpeed;
+    if (catx < 0) {
+      catx = width - 260;
+    }
+  } else if (key == 'd' || key == 'D') {
+    catx = catx + catSpeed;
+    if (catx >= width - 260) {
+      catx = 0;
+    }
+  }
 
 }
 
 function keyPressed() {
   if (key === 'm' || key === 'M') {
     song.setVolume(0);
-  }
-  else if (key === 'n' || key === 'N') {
+  } else if (key === 'n' || key === 'N') {
     song.setVolume(0.1);
 
   }
 }
 
-function writeText(){
+function writeText() {
   words = 75;
   textSize(words);
-  text("NYAN CAT SIMULATOR",width/2 - 425,height/2 - 300);
+  text("NYAN CAT SIMULATOR", width / 2 - 425, height / 2 - 300);
 }
