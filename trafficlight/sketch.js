44db34402c211c1377da68be2e458aa01f31f72a
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-02-28 14:56:08 -0600

    22

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -5,16 +5,73 @@
 //GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 // function at processing.org/reference.
-let time;
+let state;
+let redLightDuration, yellowLightDuration, greenLightDuration;
+let lastTimeLightChanged;
 
 function setup() {
   createCanvas(600, 600);
-  time = 0;
+  state = 1;
+  redLightDuration = 3000;
+  yellowLightDuration = 1000;
+  greenLightDuration = 3000;
+  lastTimeLightChanged = millis();
 }
 
 function draw() {
   background(255);
   drawOutlineOfLights();
+  checkIflightSwitched();
+  displayCorrectLight();
+}
+
+function checkIflightSwitched(){
+  if (state === 1){
+    if (millis() > lastTimeLightChanged + greenLightDuration){
+      state = 2;
+      lastTimeLightChanged = millis();
+    }
+  }
+  if (state === 2){
+    if (millis() > lastTimeLightChanged + yellowLightDuration){
+      state = 3;
+      lastTimeLightChanged = millis();
+    }
+  }
+  if (state === 3){
+    if (millis() > lastTimeLightChanged + redLightDuration){
+      state = 1;
+      lastTimeLightChanged = millis();
+    }
+  }
+}
+
+function displayCorrectLight(){
+  if (state === 3){
+    drawGreenLight();
+  }
+  else if (state === 1){
+    drawRedLight();
+  }
+  else if (state === 2){
+    drawYellowLight();
+  }
+}
+
+function drawGreenLight(){
+  fill(0,255,0);
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  text("GO",width/2,height/2);
+}
+
+function drawYellowLight(){
+  fill(255,255,0);
+  ellipse(width/2, height/2, 50, 50); //middle
+}
+
+function drawRedLight(){
+  fill(255,0,0);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
 }
 
 function drawOutlineOfLights() {
@@ -22,17 +79,4 @@ function drawOutlineOfLights() {
   rectMode(CENTER);
   fill(0);
   rect(width/2, height/2, 75, 200, 10);
-
-  //lights
-  if (time) {
-    fill(255,0,0);
-    ellipse(width/2, height/2 - 65, 50, 50);
-    millisecond = 1
-  }
-  fill(255,0,0);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
-  fill(255,255,0);
-  ellipse(width/2, height/2, 50, 50); //middle
-  fill(0,255,0);
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }
