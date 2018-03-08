28201055536c84066a3ce5825635a3bcf9040486
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-03-08 14:33:25 -0600

    8787

--- 1 STATES PROJECT/sketch.js
+++ 1 STATES PROJECT/sketch.js
@@ -4,25 +4,37 @@
 
 let state;
 let textX, textY;
+let storyFont;
 
-
+function preload(){
+  storyFont = loadFont("fonts/Storyboo.TTF");
+}
 function positionCanvas() {
 
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255)
   state = "StartScreen";
-  textX = CENTER/6;
-  textY = height/6;
+  textX = width/2 - 225;
+  textY = height/2 - 250;
+  textStyle(storyFont);
 }
 
 function draw() {
-  decision();
-
-}
-function decision() {
   if (state === "StartScreen") {
-    text("Welcome to the game of choices. Here your will be put on the spot with several decision that will influence the path you take with your caracture.",textX,textY);
+    decision();
   }
+}
+
+function decision() {
+  rect(width/2 - 500, height/2 - 225,1000,150);
+  textSize(30);
+  text("Welcome to the game of choices", textX, textY);
+  textSize(20);
+  text("Here your will be put on the spot with several decision that will have severe consequences as you continue with the game.",textX - 300,textY + 50);
+  text("Some decision will lead to a life of money and glory, others to your demise. So choose carfully", textX - 200, textY + 80);
+  text("Click the start button to continue", textX + 70, textY + 110);
+
 }
