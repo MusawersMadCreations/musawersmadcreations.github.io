7629abd0444c3042dacf1e256d2e5581fa093742
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-03-09 14:09:47 -0600

    3

--- 1 STATES PROJECT/sketch.js
+++ 1 STATES PROJECT/sketch.js
@@ -23,7 +23,6 @@ function setup() {
 }
 
 function draw() {
-  makeTextBox();
   if (mouseIsPressed) {
     print(mouseX, "X");
     print(mouseY, "Y");
@@ -31,11 +30,14 @@ function draw() {
   }
 
   if (state === "StartScreen") {
+    makeTextBox();
     TitleScreen();
   }
 
-  if (state === "Act1") {
+  if (state === "act1") {
     background(255);
+    makeTextBox();
+    act1();
   }
 }
 
@@ -56,6 +58,17 @@ function TitleScreen() {
 
 }
 
+function act1() {
+  button();
+  box.height = 110;
+  textSize(50);
+  text("Day 1", box.x + 15, box.y - 20);
+  textSize(18);
+  text("YAAAAAWN, you wake up and glance at your alarm clock. OH SHIT your late for school what do you .", box.x + 15, box.y + 25);
+  text("After rolling around for a while you realize you have to get up", box.x + 70, box.y + 50);
+  text("Click the start button to continue.", box.x + 350, box.y + 75);
+}
+
 function button() {
   let button = {
     x: 300,
@@ -67,7 +80,7 @@ function button() {
   }
 
   if (mouseX >= button.left && mouseX <= button.right && mouseY >= button.top && mouseY <= button.bottom && mouseIsPressed) {
-    state = "Act1"
+    state = "act1"
   }
 
   rect(button.x, button.y, 250, 70, 15);
