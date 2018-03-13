6ef4c7449732166f92a15a1926d121ae017b63cb
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-03-13 15:20:05 -0600

    222

---
+++ Clock/sketch.js
@@ -0,0 +1,18 @@
+// p5js template project - replace with project title
+// Dan Schellenberg - replace with your name
+// Feb 2, 2018 - replace with the date
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  ellipseMode(CENTER);
+  ellipse(width/2,height/2,700);
+  rect(CENTER);
+  rect(width/2,height/2 - 300, 15, 75);
+}
