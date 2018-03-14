3ea2420db5cd7977eb915f0a6af433ca76444580
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-03-14 14:43:58 -0600

    2212

--- Clock/sketch.js
+++ Clock/sketch.js
@@ -1,18 +1,42 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// Clock
+// Musawer Jalal
+// March 14, 2018
 
-
+let clockSize;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  if (width > height) {
+    clockSize = height;
+  }
+  else {
+    clockSize = width;
+  }
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(255);
-  ellipseMode(CENTER);
-  ellipse(width/2,height/2,700);
-  rect(CENTER);
-  rect(width/2,height/2 - 300, 15, 75);
+
+  push();
+  translate(width / 2, height / 2);
+  strokeWeight(8);
+  ellipse(0, 0, clockSize, clockSize);
+
+  //hour tic mark
+  strokeWeight(10);
+  strokeCap(SQUARE);
+  for (let i = 0; i < 12; i++) {
+    line(0, clockSize / 2 * 0.92, 0, clockSize / 2 * 0.75);
+    rotate(360/12);
+  }
+  strokeWeight(4);
+  strokeCap(SQUARE);
+  for (let i = 0; i < 60; i++) {
+    line(0, clockSize / 2 * 0.9, 0, clockSize / 2 * 0.8);
+    rotate(360/60);
+  }
+
+  pop();
 }
