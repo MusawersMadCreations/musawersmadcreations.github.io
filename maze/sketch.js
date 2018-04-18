3d6c843b564293bc7a9ad617baab4920636fa3b1
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-04-17 20:51:04 -0600

    sdad

--- maze/sketch.js
+++ maze/sketch.js
@@ -2,9 +2,11 @@
 // Musawer Jalal
 // April 10, 2018
 
+// Global Var.
 let state = "game"
 let direction, playerx, playery;
 let playerLocation = [];
+let wallLocation = [];
 let level1 = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
@@ -38,6 +40,7 @@ let cellSize;
 let rows = 25;
 let cols = 25;
 let lavaspeed;
+let score;
 let wallImg, ladderImg, lavaImg;
 
 function preload() {
@@ -47,19 +50,22 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(windowWidth, windowHeight);
   direction = "right";
-  cellSize = 25;
+  cellSize = 30.75;
   playerx = cellSize;
   playery = cellSize;
   speed = 2
-  lavaspeed = 30.25;
+  lavaspeed = 22.25;
   frameRate(10);
+  score = 0;
 
 }
 
 function draw() {
   background(255);
+  makeOutlines();
+  displayScore();
   drawBoardAndExpandLava();
   moveRunner();
   makeRunner();
@@ -100,15 +106,39 @@ function moveRunner() {
   if (frameCount % speed === 0) {
     if (direction === "right") {
       playerx += cellSize;
+      score += 1;
+      playerLocation.push(playerx);
+        if(playerx >= 738){
+          playerx -= cellSize;
+          score -= 1;
+        }
     }
     if (direction === "down") {
       playery += cellSize;
+      score += 1;
+      playerLocation.push();
+        if(playery >= 738){
+          playery -= cellSize;
+          score -= 1;
+        }
     }
     if (direction === "left") {
       playerx -= cellSize;
+      score += 1;
+      playerLocation.push();
+        if(playerx <= 0){
+        playerx += cellSize;
+        score -= 1;
+      }
     }
     if (direction === "up") {
       playery -= cellSize;
+      score += 1;
+      playerLocation.push();
+        if(playery <= 0){
+        playery += cellSize;
+        score -= 1;
+    }
     }
   }
 }
@@ -117,74 +147,86 @@ function moveRunner() {
 function drawBoardAndExpandLava() {
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
+// Converting level Characters to images
       if (level1[j][i] === 0) {
         image(wallImg, i * cellSize, j * cellSize, cellSize, cellSize);
+        wallLocation.push(0);
       } else if (level1[j][i] === 6) {
         image(lavaImg, i * cellSize, j * cellSize, cellSize, cellSize)
       } else if (level1[j][i] === "L") {
         image(ladderImg, i * cellSize, j * cellSize, cellSize, cellSize)
+// Expanding lava
       } else if (frameCount >= lavaspeed) {
         level1[j][0] = 6;
         level1[j][24] = 6;
         level1[0] = increaseWall
         level1[24] = increaseWall
-          if (frameCount >= lavaspeed * 2) {
-            level1[j][1] = 6;
-            level1[j][23] = 6;
-            level1[1] = increaseWall
-            level1[23] = increaseWall
-          }if (frameCount >= lavaspeed * 3) {
-             level1[j][2] = 6;
-             level1[j][22] = 6;
-             level1[2] = increaseWall
-             level1[22] = increaseWall
-           }if (frameCount >= lavaspeed * 4) {
-             level1[j][3] = 6;
-             level1[j][21] = 6;
-             level1[3] = increaseWall
-             level1[21] = increaseWall
-           }if (frameCount >= lavaspeed * 5) {
-             level1[j][4] = 6;
-             level1[j][20] = 6;
-             level1[4] = increaseWall
-             level1[20] = increaseWall
-           }if (frameCount >= lavaspeed * 6) {
-             level1[j][5] = 6;
-             level1[j][19] = 6;
-             level1[5] = increaseWall
-             level1[19] = increaseWall
-           }if (frameCount >= lavaspeed * 7) {
-             level1[j][6] = 6;
-             level1[j][18] = 6;
-             level1[6] = increaseWall
-             level1[18] = increaseWall
-           }if (frameCount >= lavaspeed *8) {
-             level1[j][7] = 6;
-             level1[j][17] = 6;
-             level1[7] = increaseWall
-             level1[17] = increaseWall
-           }if (frameCount >= lavaspeed* 9) {
-             level1[j][8] = 6;
-             level1[j][16] = 6;
-             level1[8] = increaseWall
-             level1[16] = increaseWall
-           }if (frameCount >= lavaspeed* 10) {
-             level1[j][9] = 6;
-             level1[j][15] = 6;
-             level1[9] = increaseWall
-             level1[15] = increaseWall
-           }if (frameCount >= lavaspeed * 11) {
-             level1[j][10] = 6;
-             level1[j][14] = 6;
-             level1[10] = increaseWall
-             level1[14] = increaseWall
-           }if (frameCount >= lavaspeed * 12) {
-             level1[j][11] = 6;
-             level1[j][13] = 6;
-             level1[11] = increaseWall
-             level1[13] = increaseWall
-           }
-
+        if (frameCount >= lavaspeed * 2) {
+          level1[j][1] = 6;
+          level1[j][23] = 6;
+          level1[1] = increaseWall
+          level1[23] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 3) {
+          level1[j][2] = 6;
+          level1[j][22] = 6;
+          level1[2] = increaseWall
+          level1[22] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 4) {
+          level1[j][3] = 6;
+          level1[j][21] = 6;
+          level1[3] = increaseWall
+          level1[21] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 5) {
+          level1[j][4] = 6;
+          level1[j][20] = 6;
+          level1[4] = increaseWall
+          level1[20] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 6) {
+          level1[j][5] = 6;
+          level1[j][19] = 6;
+          level1[5] = increaseWall
+          level1[19] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 7) {
+          level1[j][6] = 6;
+          level1[j][18] = 6;
+          level1[6] = increaseWall
+          level1[18] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 8) {
+          level1[j][7] = 6;
+          level1[j][17] = 6;
+          level1[7] = increaseWall
+          level1[17] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 9) {
+          level1[j][8] = 6;
+          level1[j][16] = 6;
+          level1[8] = increaseWall
+          level1[16] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 10) {
+          level1[j][9] = 6;
+          level1[j][15] = 6;
+          level1[9] = increaseWall
+          level1[15] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 11) {
+          level1[j][10] = 6;
+          level1[j][14] = 6;
+          level1[10] = increaseWall
+          level1[14] = increaseWall
+        }
+        if (frameCount >= lavaspeed * 12) {
+          level1[j][11] = 6;
+          level1[j][13] = 6;
+          level1[11] = increaseWall
+          level1[13] = increaseWall
+        }
       } else {
         noStroke()
         fill(255);
@@ -193,4 +235,16 @@ function drawBoardAndExpandLava() {
       }
     }
   }
+}
+function makeOutlines(){
+  fill(24);
+  rect(cellSize * 25,0,width,height);
+}
+
+function displayScore(){
+  //changed to string so it can run in text format
+  String(score);
+  fill(255);
+  textSize(150)
+  text(score + " Moves",cellSize * 28,200);
 }
