8306350869db4677bab3d42f2931a81846b125c3
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-04-19 14:53:52 -0600

    32

--- maze/sketch.js
+++ maze/sketch.js
@@ -40,12 +40,9 @@ let rows = 25;
 let cols = 25;
 let lavaspeed;
 let score;
-let wallImg, ladderImg, lavaImg;
-<<<<<<< HEAD
 let speed;
-=======
+let wallImg, ladderImg, lavaImg;
 let xcoord, ycoord;
->>>>>>> 9021cae78c6119a8a4a5fa31b9198bfe4d265d13
 
 function preload() {
   wallImg = loadImage("images/wall.jpg");
@@ -76,7 +73,7 @@ function draw() {
   moveRunner();
   makeRunner();
   death();
-  ladderWin()
+  ladderWin();
   winScreen();
   gameOverScreen();
   print(xcoord,ycoord);
@@ -114,7 +111,7 @@ function moveRunner() {
       if (direction === "right") {
         playerx += cellSize;
         score += 1;
-        if (playerx >= 738 || level1[xcoord][ycoord] === 0) {
+        if (playerx >= 738) { //|| level1[xcoord][ycoord] === 0
           playerx -= cellSize;
           score -= 1;
 
@@ -151,94 +148,6 @@ function moveRunner() {
 
 
 function drawBoardAndExpandLava() {
-<<<<<<< HEAD
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-// Converting level Characters to images
-      if (level1[j][i] === 0) {
-        image(wallImg, i * cellSize, j * cellSize, cellSize, cellSize);
-        wallLocation.push(0);
-      } else if (level1[j][i] === 6) {
-        image(lavaImg, i * cellSize, j * cellSize, cellSize, cellSize);
-      } else if (level1[j][i] === "L") {
-        image(ladderImg, i * cellSize, j * cellSize, cellSize, cellSize);
-// Expanding lava
-      } else if (frameCount >= lavaspeed) {
-        level1[j][0] = 6;
-        level1[j][24] = 6;
-        level1[0] = increaseWall;
-        level1[24] = increaseWall;
-        if (frameCount >= lavaspeed * 2) {
-          level1[j][1] = 6;
-          level1[j][23] = 6;
-          level1[1] = increaseWall
-          level1[23] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 3) {
-          level1[j][2] = 6;
-          level1[j][22] = 6;
-          level1[2] = increaseWall
-          level1[22] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 4) {
-          level1[j][3] = 6;
-          level1[j][21] = 6;
-          level1[3] = increaseWall
-          level1[21] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 5) {
-          level1[j][4] = 6;
-          level1[j][20] = 6;
-          level1[4] = increaseWall
-          level1[20] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 6) {
-          level1[j][5] = 6;
-          level1[j][19] = 6;
-          level1[5] = increaseWall
-          level1[19] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 7) {
-          level1[j][6] = 6;
-          level1[j][18] = 6;
-          level1[6] = increaseWall
-          level1[18] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 8) {
-          level1[j][7] = 6;
-          level1[j][17] = 6;
-          level1[7] = increaseWall
-          level1[17] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 9) {
-          level1[j][8] = 6;
-          level1[j][16] = 6;
-          level1[8] = increaseWall
-          level1[16] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 10) {
-          level1[j][9] = 6;
-          level1[j][15] = 6;
-          level1[9] = increaseWall
-          level1[15] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 11) {
-          level1[j][10] = 6;
-          level1[j][14] = 6;
-          level1[10] = increaseWall
-          level1[14] = increaseWall
-        }
-        if (frameCount >= lavaspeed * 12) {
-          level1[j][11] = 6;
-          level1[j][13] = 6;
-          level1[11] = increaseWall
-          level1[13] = increaseWall
-        }
-      } else {
-        noStroke()
-        fill(255);
-        rect(i * cellSize, j * cellSize, cellSize, cellSize);
-=======
   if (state === "game") {
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
@@ -325,7 +234,6 @@ function drawBoardAndExpandLava() {
           noStroke()
           fill(255);
           rect(i * cellSize, j * cellSize, cellSize, cellSize);
->>>>>>> 9021cae78c6119a8a4a5fa31b9198bfe4d265d13
 
         }
       }
